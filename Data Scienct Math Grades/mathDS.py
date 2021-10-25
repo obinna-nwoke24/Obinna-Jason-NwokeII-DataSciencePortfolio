14805d8203d160c70be2a121c4357e27cfdb28fa
Obinna Jason Nwoke II <jason.nwoke@yahoo.com>
2021-10-24 20:47:53 -0400

    First commit: jupyter notebook with visualizations

---
+++ Data Scienct Math Grades/mathDS.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+file = 'Assets/2006_-_2011_NYS_Math_Test_Results_by_Grade_-_Citywide_-_by_Race-Ethnicity.csv'
+
+
+def generate(df=pd.read_csv(file)):
+    return df
+
