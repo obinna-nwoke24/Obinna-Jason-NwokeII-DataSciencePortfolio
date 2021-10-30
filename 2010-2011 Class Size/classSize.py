b6c69afa257a71a03f6361fa6d1b399c9b67cc7e
obinna-nwoke24 <jason.nwoke@yahoo.com>
2021-10-29 21:47:57 -0400

    Committing any changed files. Starting new project - 10/28/21

---
+++ 2010-2011 Class Size/classSize.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+file = './files/2010-2011_Class_Size_-_District-level_Summary.csv'
+
+
+def generate(df=pd.read_csv(file)):
+    return df
+
---
+++ Appeals Filed in 2017/appeals.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+file = './Assets/Appeals_Filed_In_2017.csv'
+
+
+def generate(df=pd.read_csv(file)):
+    return df
